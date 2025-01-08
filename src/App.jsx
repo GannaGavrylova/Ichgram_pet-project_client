cc97497462e24024ca77ae35454070fbe707f115
GannaGavrylova <gannadeutsch@gmail.com>
2025-01-08 09:01:20 +0100

    customizing routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserAuthForm from "./components/userAuthForm";
 import UserFormRegister from "./components/userFormRegister";
 
 function App() {
   return (
-    <>
-      <UserAuthForm />
-      <UserFormRegister />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/auth/login" element={<UserAuthForm />} />
+        <Route path="/auth/register" element={<UserFormRegister />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/userFormRegister/index.jsx
+++ src/components/userFormRegister/index.jsx
@@ -38,108 +38,112 @@ function UserFormRegister() {
   };
 
   return (
-    <div className={styles.userRegisterContainer}>
-      <header className={styles.header}>
-        <img src={header} alt="header" />
-        <h3 className={styles.headerTitle}>
-          Sign up to see photos and videos from your friends.
-        </h3>
-      </header>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.formContainer}>
-        <div>
-          <input
-            type="email"
-            placeholder="Email"
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: "Invalid email format",
-              },
-            })}
-          />
-          {errors.email && (
-            <p style={{ color: "red" }}>{errors.email.message}</p>
-          )}
-        </div>
-
-        <div>
-          <input
-            type="text"
-            placeholder="Full Name"
-            {...register("fullname", {
-              required: "Full Name is required",
-              minLength: {
-                value: 2,
-                message: "Full Name must be at least 2 characters",
-              },
-            })}
-          />
-          {errors.fullname && (
-            <p style={{ color: "red" }}>{errors.fullname.message}</p>
-          )}
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            {...register("username", {
-              required: "Username is required",
-              minLength: {
-                value: 3,
-                message: "Username must be at least 3 characters",
-              },
-            })}
-          />
-          {errors.username && (
-            <p style={{ color: "red" }}>{errors.username.message}</p>
-          )}
-        </div>
-
-        <div>
-          <input
-            type="password"
-            placeholder="Password"
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 3,
-                message: "Password must be at least 3 characters",
-              },
-            })}
-          />
-          {errors.password && (
-            <p style={{ color: "red" }}>{errors.password.message}</p>
-          )}
-        </div>
-
-        <div className={styles.cookie}>
-          <p>
-            People who use service may have uploaded your contact information to
-            Instagram. <a href="#">Learn More</a>
-          </p>
-          <p>
-            By signing up, you agree to our
-            <a href="#"> Terms, Privacy Policy </a> and
-            <a href="#"> Cookies Policy</a>
-          </p>
-        </div>
-        <Button
-          type="primary"
-          htmlType="submit"
-          block
-          style={{ maxWidth: "75%", marginBottom: "30px" }}
+    <>
+      <div className={styles.userRegisterContainer}>
+        <header className={styles.header}>
+          <img src={header} alt="header" />
+          <h3 className={styles.headerTitle}>
+            Sign up to see photos and videos from your friends.
+          </h3>
+        </header>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className={styles.formContainer}
         >
-          Sign up
-        </Button>
-      </form>
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Invalid email format",
+                },
+              })}
+            />
+            {errors.email && (
+              <p style={{ color: "red" }}>{errors.email.message}</p>
+            )}
+          </div>
 
+          <div>
+            <input
+              type="text"
+              placeholder="Full Name"
+              {...register("fullname", {
+                required: "Full Name is required",
+                minLength: {
+                  value: 2,
+                  message: "Full Name must be at least 2 characters",
+                },
+              })}
+            />
+            {errors.fullname && (
+              <p style={{ color: "red" }}>{errors.fullname.message}</p>
+            )}
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Username"
+              {...register("username", {
+                required: "Username is required",
+                minLength: {
+                  value: 3,
+                  message: "Username must be at least 3 characters",
+                },
+              })}
+            />
+            {errors.username && (
+              <p style={{ color: "red" }}>{errors.username.message}</p>
+            )}
+          </div>
+
+          <div>
+            <input
+              type="password"
+              placeholder="Password"
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 3,
+                  message: "Password must be at least 3 characters",
+                },
+              })}
+            />
+            {errors.password && (
+              <p style={{ color: "red" }}>{errors.password.message}</p>
+            )}
+          </div>
+
+          <div className={styles.cookie}>
+            <p>
+              People who use service may have uploaded your contact information
+              to Instagram. <a href="#">Learn More</a>
+            </p>
+            <p>
+              By signing up, you agree to our
+              <a href="#"> Terms, Privacy Policy </a> and
+              <a href="#"> Cookies Policy</a>
+            </p>
+          </div>
+          <Button
+            type="primary"
+            htmlType="submit"
+            block
+            style={{ maxWidth: "75%", marginBottom: "30px" }}
+          >
+            Sign up
+          </Button>
+        </form>
+      </div>
       <div className={styles.questionContainer}>
         <h3 className={styles.question}>
           Have an account? <a href="#">Log in</a>
         </h3>
       </div>
-    </div>
+    </>
   );
 }
 
